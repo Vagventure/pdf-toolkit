86f90ce727a0a39206536d4183d596d3bd9a11fe
Vagventure <ankitverma0366@gmail.com>
2025-07-04 18:56:24 +0530

    Added to images conversions

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import render_template
 from flask import request
 from flask import send_from_directory
 from werkzeug.utils import secure_filename
+import zipfile
 import os
 import subprocess
 
@@ -19,43 +20,240 @@ def hello_world():
 
 @app.route("/tools/<slug>", methods=['GET','POST'])
 def pdf_tool(slug):
-    if request.method == 'GET':
-        return render_template("tooljinja.html", name=escape(slug) , operation="compress pdf")
+    op = escape(slug)
    
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-           fileName = secure_filename(file.filename)
-           Upload_path = os.path.join(UPLOAD_FOLDER,fileName)
-           output_fileName = f"Processed_{fileName}"
-           Output_path = os.path.join(OUTPUT_FOLDER,output_fileName)
-         
-           file.save(Upload_path)
-
-         # Pdf compresser 
-     
-           command = [
-              "gswin64c",
-              "-sDEVICE=pdfwrite",
-              "-dCompatibilityLevel=1.4",
-              "-dPDFSETTINGS=/screen",
-              "-dNOPAUSE",
-              "-dQUIET",
-              "-dBATCH",
-              f"-sOutputFile={Output_path}",
-             Upload_path
-            ]
-           
-           try:
-              subprocess.run(command, check=True)
-              print(f"Pdf successfully compressed to : {output_fileName}")
-              return send_from_directory(directory=OUTPUT_FOLDER, path=output_fileName, as_attachment= True)
-           except subprocess.CalledProcessError as e:
-              print("Compression failed try again : ",e)  
-              return jsonify(success=False, error="Ghostscript processing failed."), 500
+    match(op):
+       
+       case "Pdf Merger":
+           return pdf_merger(op)
         
-        return jsonify(success=False, error="File upload failed"), 400
-    return render_template("tooljinja.html", name=escape(slug) , operation="compress pdf")
+       case "Pdf Compresser":
+           return pdf_compress(op)
+       
+       case "Pdf Splitter":
+           return pdf_splitt(op)
+      
+       case "Pdf to PNG":
+           return pdf_PNG(op)
+      
+       case "Pdf to JPG":
+           return pdf_JPG(op)
+      
+       case "Pdf to TIFF":
+           return pdf_TIFF(op)
+  
+    return render_template("tooljinja.html", name=op , operation="splitt pdf")      
+         
+
+def pdf_merger(name): 
+    if request.method == 'GET':
+             return render_template("tooljinja.html", name=name , operation="compress pdf")
+
+    if request.method == 'POST':
+            uploaded_files = request.files.getlist('files[]')
+            if uploaded_files:
+               for file in uploaded_files:
+                 fileName = secure_filename(file.filename)
+                 Upload_path = os.path.join(UPLOAD_FOLDER,fileName)
+                 output_fileName = f"Processed_{fileName}"
+                 Output_path = os.path.join(OUTPUT_FOLDER,output_fileName)
+                 
+                 file.save(Upload_path)
+                
+            input_files = os.listdir(UPLOAD_FOLDER)
+            #Ghost Command
+            command = [
+               "gswin64c",
+               "-sDEVICE=pdfwrite",
+               "-dBATCH",
+               "-dQUIET",
+               "-dNOPAUSE",
+               f"-sOutputFile={Output_path}"
+            ] + input_files
+            
+            try:
+               subprocess.run(command, check=True)
+               print("Pdfs are successfully merged : ",output_fileName)
+               return send_from_directory(directory=OUTPUT_FOLDER, path=output_fileName, as_attachment= True)
+
+            except Exception as e:
+               print("Merging failed try again : ",e) 
+               return jsonify(success=False, error="Ghostscript processing failed."), 500
+
+    return jsonify(success=False, error="File upload failed"), 400  
+    
+
+
+def pdf_compress(name):
+         if request.method == 'GET':
+             return render_template("tooljinja.html", name=name , operation="compress pdf")
+        
+         if request.method == 'POST':
+             uploaded_files = request.files.getlist('files[]')
+             if uploaded_files:
+                for file in uploaded_files:
+                 fileName = secure_filename(file.filename)
+                 Upload_path = os.path.join(UPLOAD_FOLDER,fileName)
+                 output_fileName = f"Processed_{fileName}"
+                 Output_path = os.path.join(OUTPUT_FOLDER,output_fileName)
+                 
+                 file.save(Upload_path)
+     
+              # Pdf compresser 
+          
+                command = [
+                   "gswin64c",
+                   "-sDEVICE=pdfwrite",
+                   "-dCompatibilityLevel=1.4",
+                   "-dPDFSETTINGS=/ebook",
+                   "-dNOPAUSE",
+                   "-dQUIET",
+                   "-dBATCH",
+                   f"-sOutputFile={Output_path}",
+                  Upload_path
+                 ]
+                
+                try:
+                   subprocess.run(command, check=True)
+                   print(f"Pdf successfully compressed to : {output_fileName}")
+                   return send_from_directory(directory=OUTPUT_FOLDER, path=output_fileName, as_attachment= True)
+                except subprocess.CalledProcessError as e:
+                   print("Compression failed try again : ",e)  
+                   return jsonify(success=False, error="Ghostscript processing failed."), 500
+                
+         return jsonify(success=False, error="File upload failed"), 400  
+              
+def pdf_splitt(name):
+   if request.method == 'GET':
+       return render_template("tooljinja.html", name=name , operation="splitt pdf")
+  
+def pdf_PNG(name):
+    if request.method == 'GET':
+             return render_template("tooljinja.html", name=name , operation="Pdf to PNG")
+
+    if request.method == 'POST':
+            uploaded_files = request.files.getlist('files[]')
+            if uploaded_files:
+               for file in uploaded_files:
+                 fileName = secure_filename(file.filename)
+                 Upload_path = os.path.join(UPLOAD_FOLDER,fileName)
+                 output_fileName = "img"
+                 Output_path = os.path.join(OUTPUT_FOLDER,output_fileName)
+                 
+                 file.save(Upload_path)
+                
+          #Ghostscript command
+               command = [
+                  "gswin64c",
+                  "-sDEVICE=png16m",
+                  f"-r150",
+                  f"-dDownScaleFactor=2",
+                  "-dNOPAUSE",
+                  "-dQUIET",
+                  "-dBATCH",
+                  f"-sOutputFile={Output_path}%03d.png",
+                  Upload_path
+                ]
+              
+               try:
+                  subprocess.run(command, check=True)
+                  zip_path = os.path.join(OUTPUT_FOLDER,"photos.zip")
+                  with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                      for image in os.listdir(OUTPUT_FOLDER):
+                          if image.lower().endswith(('.png', '.jpg', '.jpeg', '.webp', '.gif')):
+                              full_path = os.path.join(OUTPUT_FOLDER,image)
+                              zipf.write(full_path, arcname=image)
+                  print(f"Photos zipped into: {output_fileName}")
+                  return send_from_directory(directory=OUTPUT_FOLDER, path="photos.zip", as_attachment= True)
+
+               except subprocess.CalledProcessError as e:
+                  print("Conversion failed try again: ",e)    
+
+def pdf_JPG(name):
+    if request.method == 'GET':
+             return render_template("tooljinja.html", name=name , operation="Pdf to JPG")
+
+    if request.method == 'POST':
+            uploaded_files = request.files.getlist('files[]')
+            if uploaded_files:
+               for file in uploaded_files:
+                 fileName = secure_filename(file.filename)
+                 Upload_path = os.path.join(UPLOAD_FOLDER,fileName)
+                 output_fileName = "img"
+                 Output_path = os.path.join(OUTPUT_FOLDER,output_fileName)
+                 
+                 file.save(Upload_path)
+                
+          #Ghostscript command
+               command = [
+                  "gswin64c",
+                  "-sDEVICE=jpeg",
+                  f"-r150",
+                  f"-dDownScaleFactor=2",
+                  "-dNOPAUSE",
+                  "-dQUIET",
+                  "-dBATCH",
+                  f"-sOutputFile={Output_path}%03d.JPG",
+                  Upload_path
+                ]
+              
+               try:
+                  subprocess.run(command, check=True)
+                  zip_path = os.path.join(OUTPUT_FOLDER,"photos.zip")
+                  with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                      for image in os.listdir(OUTPUT_FOLDER):
+                          if image.lower().endswith(('.png', '.jpg', '.jpeg', '.webp', '.gif')):
+                              full_path = os.path.join(OUTPUT_FOLDER,image)
+                              zipf.write(full_path, arcname=image)
+                  print(f"Photos zipped into: {output_fileName}")
+                  return send_from_directory(directory=OUTPUT_FOLDER, path="photos.zip", as_attachment= True)
+
+               except subprocess.CalledProcessError as e:
+                  print("Conversion failed try again: ",e)    
+
+def pdf_TIFF(name):
+    if request.method == 'GET':
+             return render_template("tooljinja.html", name=name , operation="Pdf to TIFF")
+
+    if request.method == 'POST':
+            uploaded_files = request.files.getlist('files[]')
+            if uploaded_files:
+               for file in uploaded_files:
+                 fileName = secure_filename(file.filename)
+                 Upload_path = os.path.join(UPLOAD_FOLDER,fileName)
+                 output_fileName = "img"
+                 Output_path = os.path.join(OUTPUT_FOLDER,output_fileName)
+                 
+                 file.save(Upload_path)
+                
+          #Ghostscript command
+               command = [
+                  "gswin64c",
+                  "-sDEVICE=tiff24nc",
+                  "-r150",
+                  "-dNOPAUSE",
+                  "-dQUIET",
+                  "-dBATCH",
+                  f"-sOutputFile={Output_path}%03d.tiff",
+                  Upload_path
+                ]
+              
+               try:
+                  subprocess.run(command, check=True)
+                  zip_path = os.path.join(OUTPUT_FOLDER,"photos.zip")
+                  with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                      for image in os.listdir(OUTPUT_FOLDER):
+                          if image.lower().endswith(('.png', '.jpg', '.jpeg', '.webp', '.gif')):
+                              full_path = os.path.join(OUTPUT_FOLDER,image)
+                              zipf.write(full_path, arcname=image)
+                  print(f"Photos zipped into: {output_fileName}")
+                  return send_from_directory(directory=OUTPUT_FOLDER, path="photos.zip", as_attachment= True)
+
+               except subprocess.CalledProcessError as e:
+                  print("Conversion failed try again: ",e)    
+
+            return jsonify(success=False, error="File upload failed"), 400
+
 
 # @app.route('/tools/<slug>')
 # def template(slug):
--- static/script.js
+++ static/script.js
@@ -1,44 +1,57 @@
-let selectedToolSlug = null;
+// let text = null
 
-document.querySelectorAll(".tools").forEach(e => {
-    e.addEventListener('click', () => {
-        selectedToolSlug = e.innerText.trim();
-        console.log("Selected tool:", selectedToolSlug);
-        window.open(`/tools/${encodeURIComponent(selectedToolSlug)}`, '_blank');
-    });
-});
+document.addEventListener("DOMContentLoaded", async function () {
+    document.querySelectorAll(".tools").forEach(e => {
+        e.addEventListener('click', (p) => {
+            let text = p.target.innerText.trim();  // or e.innerText
+            console.log(text);
+            window.open(`/tools/${encodeURIComponent(text)}`, '_blank')
 
-fileInput.addEventListener('change', () => {
-    if (!selectedToolSlug) {
-        alert("Please select a tool first.");
-        return;
-    }
+            const form = document.getElementById('upload-form')
+            const fileInput = document.getElementById('file-upload');
+            const progressBar = document.getElementById('upload-progress');
+            fileInput.addEventListener('change', () => {
+                const files = fileInput.files;
+                if (!files) return;
+    
+    
+                const xhr = new XMLHttpRequest()
+                const formData = new FormData()
+                
+                for (let i = 0; i < files.length; i++) {
+                    formData.append('files[]', files[i])
+                }
+    
+                xhr.open('POST', `/tools/${text}`, true)
+    
+                progressBar.classList.remove('hidden')
+    
+                xhr.upload.onprogress = (e) => {
+                    if (e.lengthComputable) {
+                        const percent = (e.loaded / e.total) * 100
+                        progressBar.value = percent
+                    }
+                }
+    
+                xhr.onload = () => {
+                    if (xhr.status === 200) {
+                        alert("Upload complete")
+                        progressBar.value = 100
+    
+                    } else {
+                        alert("Upload failed")
+                    }
+                }
+    
+                xhr.send(formData)
+            })
+    
+            setInterval(() => {
+                sig = document.querySelector(".blinker")
+                sig.classList.toggle('hidden')
+    
+            }, 500);
+        });
+        });
 
-    const file = fileInput.files[0];
-    if (!file) return;
-
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData();
-    formData.append('file', file);
-
-    xhr.open('POST', `/tools/${encodeURIComponent(selectedToolSlug)}`, true);
-
-    progressBar.classList.remove('hidden');
-
-    xhr.upload.onprogress = (e) => {
-        if (e.lengthComputable) {
-            progressBar.value = (e.loaded / e.total) * 100;
-        }
-    };
-
-    xhr.onload = () => {
-        if (xhr.status === 200) {
-            alert("Upload complete");
-            progressBar.value = 100;
-        } else {
-            alert("Upload failed");
-        }
-    };
-
-    xhr.send(formData);
 });
